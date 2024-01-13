919288cbd675397ad62232d1a2eaeb314453c837
Gon_160 <8goncaloalvesgomes@gmail.com>
2024-01-13 16:36:32 +0000

    erro na linha 33 "MAtual[i] = MAtual[i-1] + SAtual[i]" MAtual[i-1] fora de range

--- WorkspacePython/Ficha 3/F3_Ex6.py
+++ WorkspacePython/Ficha 3/F3_Ex6.py
@@ -10,16 +10,45 @@
     #e. Ler o nome e o salário atual do próximo funcionário
 #5. Imprimir as massas salariais se o contador de funcionário for maior do que zero
 
-Conta = int(input("Diga a quantidade de funcionários: "))
+Nomes, SAtual, SFuturo,MAtual ,MFuturo = [], [], [], [], []
+Conta = 0
 
-while i < Conta:
+while True:
     Nome = str(input("Diga o nome do funcionário (ZZZ para terminar): "))
-    if Nome.upper == "ZZZ"
+    if Nome.upper() == "ZZZ":
         break
-    MAtual = float(input("Diga o salário atual do funcionário: "))
+    Nomes.append(Nome)
+    Dinheiro = float(input("Diga o salário atual do funcionário: "))
+    while Dinheiro < 0:
+        if Dinheiro < 0:
+            print("Salário invalido")
+            print("O salário precisa ser 0 ou maior")
+            Dinheiro = float(input("Diga o salário atual do funcionário: "))
+        else:
+            break
+    SAtual.append(Dinheiro)
+    Conta += 1
 
-print("Nome", end="")
-for i in range(1, Conta+1, 1):
-    print(f'{Nome}')
-    for N in 4:
-        print()
+for i in range(0, Conta, 1):
+    MAtual[i] = MAtual[i] + SAtual[i]
+    print(f'MFuturo:  {MAtual[i]}')
+
+for i in range(0, Conta, 1):
+    if SAtual[i] >= 0 and SAtual[i] <= 500:
+        SFuturo[i] = SAtual[i] * 0.1
+        MFuturo[i] = MFuturo[i-1] + SAtual[i]
+        print(f'MFuturo:  {MFuturo[i]}')
+    elif SAtual[i] >= 501 and SAtual[i] <= 800:
+        SFuturo[i] = SAtual[i] * 0.05
+        MFuturo[i] = MFuturo[i-1] + SAtual[i]
+        print(f'MFuturo:  {MFuturo[i]}')
+    else:
+        SFuturo[i] = SAtual[i]
+        MFuturo[i] = MFuturo[i-1] + SAtual[i]
+        print(f'MFuturo:  {MFuturo[i]}')
+
+#print("Nome", end="")
+#for i in range(1, Conta+1, 1):
+#    print(f'{Nome}')
+#    for N in 4:
+#        print()
