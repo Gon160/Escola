fbc145ea3dcd955381cccd6cca5663fbc732fa6b
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-15 13:08:35 +0000

    ns

--- WorkspacePython/Ficha 3/F3_Ex4.py
+++ WorkspacePython/Ficha 3/F3_Ex4.py
@@ -9,6 +9,7 @@ print(f'{Inicial:5d} €', end="")
 for i in Taxa:
     print(f'{Esp}{i/100:^11.1%}', end="")
 print()
+
 for N in range(1, Anos+1, 1):
     print(f'{AnoInicio+N:5d}', end="")
     for i in Taxa:
