26ff92505f0f62dc0ce2a1944a1d957d0cdc105c
Gon_160 <8goncaloalvesgomes@gmail.com>
2024-01-16 21:20:39 +0000

    F#_Ex8 terminado e funcional

--- WorkspacePython/Ficha 3/F3_Ex8.py
+++ WorkspacePython/Ficha 3/F3_Ex8.py
@@ -4,11 +4,15 @@
 # 2. Desenhar duas linhas verticais, uma em cada extremidade da linha do topo, para representarem a largura do retângulo
 # 3. Desenhar a linha do fundo do retângulo, correspondente ao comprimento.
 
-Comp = int(input('Digite o comprimento: '))
 Larg = int(input('Digite o largura: '))
+Comp = int(input('Digite o comprimento: '))
 
-for l in range(1, Larg+1):
-    print("X")
-    for c in range(1, Comp+1):
-        print("X", end="")
+for l in range(1, Larg + 1):
+    for c in range(1, Comp + 1):
+        if l == 1 or l == Larg:
+            print("X", end="")
+        elif c == 1 or c == Comp:
+            print("X", end="")
+        else:
+            print(" ", end="")
     print()
