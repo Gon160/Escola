2779ce55f31dabc4de8a5ad6cc579a55085a8c32
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-18 17:26:33 +0000

    F4_Ex10 terminado e funcional

---
+++ WorkspacePython/Ficha 4/F4_Ex10.py
@@ -0,0 +1,22 @@
+Equipa_1, Equipa_2, Equipa_3, Equipa_4 = [], [],[], []
+
+for i in range(0, 3):
+    Nome = str(input("Digite o nome do participante da equipa 1: "))
+    Equipa_1.append(Nome)
+
+for i in range(0, 3):
+    Nome = str(input("Digite o nome do participante da equipa 2: "))
+    Equipa_2.append(Nome)
+
+for i in range(0, 3):
+    Nome = str(input("Digite o nome do participante da equipa 3: "))
+    Equipa_3.append(Nome)
+
+for i in range(0, 3):
+    Nome = str(input("Digite o nome do participante da equipa 4: "))
+    Equipa_4.append(Nome)
+
+print(f'Equipa 1 = {Equipa_1}')
+print(f'Equipa 2 = {Equipa_2}')
+print(f'Equipa 3 = {Equipa_3}')
+print(f'Equipa 4 = {Equipa_4}')
