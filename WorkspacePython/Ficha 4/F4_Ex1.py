9c7f591f596c3797989e4838fb8e1ae68eb0b339
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-18 17:19:58 +0000

    F4_Ex1 terminado e funcional

---
+++ WorkspacePython/Ficha 4/F4_Ex1.py
@@ -0,0 +1,10 @@
+#for i in range(11,1,-1):
+#   print(" "*int(11-i), end='')
+#   for j in range(1,i):
+#       print(11- j,'', end='' )
+#   print()
+
+
+#- [ ] Imprime uma árvore feita de números em ordem crescente.
+#- [X] Imprime um arvore invertida com números em ordem decrescente
+#- [ ] Imprime uma árvore invertida com números em ordem crescente
