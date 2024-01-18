11ea314835d8dde50e99f66d5e3cfa1f40a2266e
Gon_160 <8goncaloalvesgomes@gmail.com>
2024-01-18 22:37:24 +0000

    F4_Ex10 terminado e funcional

--- WorkspacePython/Ficha 4/F4_Ex10.py
+++ WorkspacePython/Ficha 4/F4_Ex10.py
@@ -1,22 +1,8 @@
-Equipa_1, Equipa_2, Equipa_3, Equipa_4 = [], [],[], []
+Jogadores = ["Ana", "Rui", "Telmo", "Beatriz", "Luisa", "Joana", "Manuel", "Pedro", "Paulo", "Paulino", "Maria", "Teresa"]
 
-for i in range(0, 3):
-    Nome = str(input("Digite o nome do participante da equipa 1: "))
-    Equipa_1.append(Nome)
+Contador = 0
 
-for i in range(0, 3):
-    Nome = str(input("Digite o nome do participante da equipa 2: "))
-    Equipa_2.append(Nome)
-
-for i in range(0, 3):
-    Nome = str(input("Digite o nome do participante da equipa 3: "))
-    Equipa_3.append(Nome)
-
-for i in range(0, 3):
-    Nome = str(input("Digite o nome do participante da equipa 4: "))
-    Equipa_4.append(Nome)
-
-print(f'Equipa 1 = {Equipa_1}')
-print(f'Equipa 2 = {Equipa_2}')
-print(f'Equipa 3 = {Equipa_3}')
-print(f'Equipa 4 = {Equipa_4}')
+while Contador < len(Jogadores):
+    Equipa = Jogadores[Contador:Contador + 3]
+    print(Equipa)
+    Contador = Contador + 3
