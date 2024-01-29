771f37ec1a9543092e6c3aa64735fedcfe333c1c
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-29 21:33:54 +0000

    F6_Ex2 terminado e funcional

--- WorkspacePython/Ficha 6/F6_Ex2.py
+++ WorkspacePython/Ficha 6/F6_Ex2.py
@@ -1,3 +1,10 @@
+#Escreva um programa que peça ao utilizador um número inteiro n entre 1 e 100,
+#inclusive. A leitura dos dados e respetiva validação devem ser feitas numa função com
+#o seguinte protótipo:
+#def readData()
+#que devolve o inteiro lido. O programa deve mostrar no ecrã os múltiplos de 3, desde n
+#até 121, usando para isso uma função com o seguinte protótipo:
+#def multipleOf3(n).
 def readData():
     while True:
         x = int(input("Insira um numero entre 1 e 121: "))
