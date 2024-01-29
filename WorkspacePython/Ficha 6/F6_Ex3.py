ea3ca4e26acfc6de1208e96c1fab8e5455ffbd1c
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-29 21:33:40 +0000

    F6_Ex3 terminado e funcional

--- WorkspacePython/Ficha 6/F6_Ex3.py
+++ WorkspacePython/Ficha 6/F6_Ex3.py
@@ -1,3 +1,11 @@
+#Escreva um programa que peça ao utilizador um carater n que deverá corresponder a
+#uma letra apenas (maiúscula ou minúscula). A leitura dos dados e respetiva validação
+#devem ser feitas numa função que devolve o carater lido e com o seguinte protótipo:
+#def readData()
+#Se a letra introduzida é minúscula, o programa deve mostrar no ecrã a correspondente
+#letra maiúscula e vice-versa. Deve usar, para esse efeito, uma função que aceita e
+#devolve valores do tipo char, com o seguinte protótipo:
+#def changeCase(n)
 def readData():
     while True:
         x = str(input("Insira um caractere de A-Z: "))
@@ -6,6 +14,7 @@ def readData():
             break
         else:
             print("Carectere invalido volte a tentar")
+
 
 def chanceCase(x):
     if x.islower():
