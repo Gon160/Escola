f02c2e2941aba1ed7af131183440c087620ebabf
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-29 21:11:11 +0000

    F6_Ex5 terminado e funcional

--- WorkspacePython/Ficha 6/F6_Ex5.py
+++ WorkspacePython/Ficha 6/F6_Ex5.py
@@ -1,16 +1,51 @@
+#Escreva um programa que peça ao utilizador dois números reais, m e n, entre -100 e
+#100, inclusive. Faça a leitura e validação de cada número à vez, usando uma função com
+#o protótipo incluído a seguir, e que deve pedir para o utilizador introduzir um número
+#no intervalo pretendido e devolver o número lido, após a validação do intervalo:
+#def readNumber()
+
+#Depois, através de uma função com o protótipo
+#def menu()
+#deve mostrar o seguinte menu e devolver a opção (validada), que é um número inteiro,
+#escolhida pelo utilizador:
+
+#1 - Show the largest value.
+#2 - Show the smallest value.
+#3 - Show the average value.
+#0 - End.
+
+#Finalmente, as opções listadas no menu devem ser implementadas sob a forma de
+#funções. Estas funções são invocadas de acordo com a escolha do utilizador. O programa
+#deve ficar em ciclo até a opção 0 ser escolhida. Considere o seguinte exemplo de
+#execução:
+
+#Insert real number a (between -100 and 100): -120.5
+#** NUMBER OUT OF RANGE. Try again! **
+#Insert real number a (between -100 and 100): -3
+#Insert real number a (between -100 and 100): 1
+
+#1- Show the largest value.
+#2- Show the smallest value.
+#3- Show the average value.
+#0- End.
+
+#1
+#The largest value is 1.
+
+#1- Show the largest value.
+#2- Show the smallest value.
+#3- Show the average value.
+#0- End.
+
+#0
+#Exiting.
+
+
 def readNumber():
     while True:
-        m = int(input("Insira um numero entr e -100 e 100: "))
+        m = int(input("Insira um numero entre -100 e 100: "))
         if -100 <= m <= 100:
             return m
-            break
-        else:
-            print("Valor errado volte a tentar")
-
-    while True:
-        n = int(input("Insira um numero entre -100 e 100: "))
-        if -100 <= n <= 100:
-            return n
             break
         else:
             print("Valor errado volte a tentar")
@@ -26,16 +61,16 @@ def menu():
         match op:
             case 1:
                 if m > n:
-                    print(m)
+                    print("O maior valor é: ", m)
                 else:
-                    print(n)
+                    print("O maior valor é: ", n)
             case 2:
                 if m < n:
-                    print(m)
+                    print("O menor valor é: ", m)
                 else:
-                    print(n)
+                    print("O menor valor é: ", n)
             case 3:
-                print((m + n) / 2)
+                print("A mendia dos valores é: ", (m+n)/2)
             case 4:
                 print("Programa finalizado")
                 break
