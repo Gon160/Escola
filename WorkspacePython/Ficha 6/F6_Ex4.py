e7fd4e2a68571d253eacdf58512e056e81135152
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-29 21:11:28 +0000

    F6_Ex4 terminado e funcional

--- WorkspacePython/Ficha 6/F6_Ex4.py
+++ WorkspacePython/Ficha 6/F6_Ex4.py
@@ -1,3 +1,9 @@
+#Modifique o programa anterior de forma a que sejam mostradas no ecrã todas as letras
+#desde A até à letra introduzida, inclusive, usando para isso uma função com o seguinte
+#protótipo:
+#def fromAtoINPUT(n)
+#As letras mostradas devem ser todas maiúsculas independentemente da letra
+#introduzida ser ou não maiúscula. Considere o seguinte exemplo de execução:
 def readData():
     while True:
         x = str(input("Insira um caractere de A-Z: "))
