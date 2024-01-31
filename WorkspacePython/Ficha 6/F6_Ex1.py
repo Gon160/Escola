40ae0f7436396e137092da24ddd1ff7874f0c752
Gon160 <8goncaloalvesgomes@gmail.com>
2024-01-31 16:52:31 +0000

    F6_Ex1 terminado e funcional

--- WorkspacePython/Ficha 6/F6_Ex1.py
+++ WorkspacePython/Ficha 6/F6_Ex1.py
@@ -8,12 +8,14 @@ def soma(x):
     valor = 0
     for i in range(x + 1):
         valor += x + i ** 2
-    return valor
+    print(f'Soma = {valor}')
 
 
 while True:
     x = int(input("Insira um número entre 1 e 100: "))
     if 1 <= x <= 100:
         break
+    else:
+        print("Valor invalido!")
 
-print(f'Soma = {soma(x)}')
+soma(x)
