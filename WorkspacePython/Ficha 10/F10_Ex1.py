8e4ca08733dd28af7d0acba2fafc60b31084db11
Gon160 <8goncaloalvesgomes@gmail.com>
2024-02-22 17:26:38 +0000

    enviar logo tudo msm q tenha algo por acabar

--- WorkspacePython/Ficha 10/F10_Ex1.py
+++ WorkspacePython/Ficha 10/F10_Ex1.py
@@ -19,6 +19,7 @@ def contaSalarioLA():
     salarioL = SalarioA - valorIRS - valorSS
     return salarioL
 
+
 SalarioI = float(input("Digite o seu salario atual: "))
 IRS = float(input("Qual o valor q paga do IRS em precentagem: "))
 SS = float(input("Qual o valor q paga da Segurança Social em precentagem: "))
