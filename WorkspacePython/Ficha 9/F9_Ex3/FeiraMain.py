a69e48ed0e04b09fb919d088a04315d8e49fe762
Gon160 <8goncaloalvesgomes@gmail.com>
2024-02-22 10:36:07 +0000

    enviar logo tudo msm q tenha algo por acabar

--- WorkspacePython/Ficha 9/F9_Ex3/FeiraMain.py
+++ WorkspacePython/Ficha 9/F9_Ex3/FeiraMain.py
@@ -1,28 +1,28 @@
 from Feira import feira_class
 
-livro_0 = feira_class("Cem Anos de Solidão, por Gabriel García Márquez")
-livro_1 = feira_class("1984, George Orwell")
-livro_2 = feira_class("O Apanhador no Campo de Centeio, J.D. Salinger")
-livro_3 = feira_class("Harry Potter e a Pedra Filosofal, J.K. Rowling")
+produto_0 = feira_class("Cem Anos de Solidão, por Gabriel García Márquez")
+produto_1 = feira_class("1984, George Orwell")
+produto_2 = feira_class("O Apanhador no Campo de Centeio, J.D. Salinger")
+produto_3 = feira_class("Harry Potter e a Pedra Filosofal, J.K. Rowling")
 
-print("Bem-vindo! Livraria Gama tem algumas ofertas para você!")
+print("Bem-vindo! A feira tem algumas ofertas para você!")
 comprador = input("Digite seu nome para começarmos: ")
-print(f"{comprador} temos 10 livros que combinam com você:"
+print(f"{comprador} temos 4 frutas para você:"
 '''
-[0] Cem Anos de Solidão, por Gabriel García Márquez
-[1] 1984, George Orwell
-[2] O Apanhador no Campo de Centeio, J.D. Salinger
-[3] Harry Potter e a Pedra Filosofal, J.K. Rowling''')
+[0] Maçã
+[1] Banana
+[2] Morango
+[3] Ananás''')
 
-selecao = int(input("Selecione o número do livro desejado: "))
-lista_livro = [livro_0, livro_1, livro_2, livro_3]
+selecao = int(input("Selecione o número da fruta desejada: "))
+lista_produtos = [produto_0, produto_1, produto_2, produto_3]
 opcao_selecionada = int(selecao)
 
-for opcao in lista_livro:
+for opcao in lista_produtos:
     if opcao_selecionada >= 5:
-        print("Esta opção não está incluída em nossos livros.")
+        print("Esta opção não está incluída em nossas frutas.")
         break
     if opcao_selecionada <= 4:
-        print(f"{comprador} seu livro {lista_livro[opcao_selecionada].emprestimo}.")
+        print(f"{comprador} comprou: {lista_produtos[opcao_selecionada].nome_fruta}.")
         print("Volte sempre!")
         break
